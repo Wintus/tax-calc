1cb3cd19062e5e3d076441d7c599c7f51fc0018c
wintus <yuya0726+GitHub@gmail.com>
2019-10-26 05:23:41 +0900

    Update test sample code

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,22 +1,36 @@
 module Tests exposing (..)
 
-import Test exposing (..)
 import Expect
+import Fuzz exposing (string)
+import Test exposing (..)
 
 
--- Check out http://package.elm-lang.org/packages/elm-community/elm-test/latest to learn more about testing in Elm!
+suite : Test
+suite =
+    describe "The String module"
+        [ describe "String.reverse"
+            -- Nest as many descriptions as you like.
+            [ test "has no effect on a palindrome" <|
+                \_ ->
+                    let
+                        palindrome =
+                            "hannah"
+                    in
+                    Expect.equal palindrome (String.reverse palindrome)
 
+            -- Expect.equal is designed to be used in pipeline style, like this.
+            , test "reverses a known string" <|
+                \_ ->
+                    "ABCDEFG"
+                        |> String.reverse
+                        |> Expect.equal "GFEDCBA"
 
-all : Test
-all =
-    describe "A Test Suite"
-        [ test "Addition" <|
-            \_ ->
-                Expect.equal 10 (3 + 7)
-        , test "String.left" <|
-            \_ ->
-                Expect.equal "a" (String.left 1 "abcdefg")
-        , test "This test should fail" <|
-            \_ ->
-                Expect.fail "failed as expected!"
+            -- fuzz runs the test 100 times with randomly-generated inputs!
+            , fuzz string "restores the original string if you run it again" <|
+                \randomlyGeneratedString ->
+                    randomlyGeneratedString
+                        |> String.reverse
+                        |> String.reverse
+                        |> Expect.equal randomlyGeneratedString
+            ]
         ]
