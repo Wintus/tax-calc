471ec828006fdd5fdd05d91b7ffb18d547045169
wintus <yuya0726+GitHub@gmail.com>
2019-11-10 02:49:02 +0900

    Updates

---
+++ precache-manifest.a35a8ecba0557b046278d75e91cb0efd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/tax-calc/favicon.ico"
+  },
+  {
+    "revision": "382fd7c2448972c2452860f76a6a76fe",
+    "url": "/tax-calc/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/tax-calc/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/tax-calc/manifest.json"
+  },
+  {
+    "revision": "c447e3a1108aad969b2b",
+    "url": "/tax-calc/static/css/main.fc26b396.chunk.css"
+  },
+  {
+    "revision": "c447e3a1108aad969b2b",
+    "url": "/tax-calc/static/js/main.c447e3a1.chunk.js"
+  },
+  {
+    "revision": "74c60d467fd1a1772361",
+    "url": "/tax-calc/static/js/runtime~main.74c60d46.js"
+  },
+  {
+    "revision": "3ca81432027bbb7b376c",
+    "url": "/tax-calc/static/js/vendors~main.3ca81432.chunk.js"
+  }
+]);
